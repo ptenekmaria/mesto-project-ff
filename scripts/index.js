79477e3904540a7510e9f66d1e6dd271f53d2c15
ptenek <ptenekmaria@yandex.ru>
2025-03-06 23:47:08 +0300

    feat: add card + delete button

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
+// DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard (item) {
+    // Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template').content;
+    const placeCard = cardTemplate.querySelector('.card').cloneNode(true);
+    placeCard.querySelector('.card__title').textContent = item.name;
+    placeCard.querySelector('.card__image').src = item.link;
+    placeCard.querySelector('.card__image').alt = item.name;
+    // добавим обработчик
+    placeCard.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return placeCard;
+}
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
+function deleteCard (evt) {
+    const cardToDelete = evt.target.closest('.card');
+    cardToDelete.remove();
+}
 
 // @todo: Вывести карточки на страницу
+const placeCards = initialCards.map(createCard);
+placeCards.forEach(function(item) {
+    placesList.append(item);
+});
